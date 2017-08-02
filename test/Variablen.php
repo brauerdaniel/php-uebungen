5edbd0bdf9fa11af20b9ff75dfba24c9acab7776
Daniel Brauer <daniel.brauer@aubi-plus.de>
2017-08-02 12:09:18 +0200

    Übungsaufgaben, siehe php tutorial

---
+++ test/Variablen.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Daniel
+ * Date: 01.08.2017
+ * Time: 15:18
+ */
+$name = "Paul Meier";
+echo "Zuerst heiße ich $name <br />";
+
+$name ="Stefan Müller";
+echo "Dann ist mein Name $name <br />";
+
+echo $name ;
+
+
+$name = "<br /> Nils ";
+$name .= "Reimers";
+echo $name;
+
+$name = "Nils ";
+echo "<br />Mein Name ist ".$name. "Reimers";
+
+$farbe = "rotes";
+$text = "<br />Wir haben ein ".$farbe. "Haus";
+echo $text;
+
---
+++ test/uebung2.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Daniel
+ * Date: 02.08.2017
+ * Time: 07:54
+ */
+$vorname = $_GET['vorname'];
+$name = $_GET['name'];
+$nachname = $_GET['nachname'];
+echo "Hallo $vorname $nachname";
+?>
